78c61e0bb18b747ff6898167e29e697ee2b65bea
George Aykara <aykarageorge@gmail.com>
2020-05-25 22:33:04 +0530

    installed express.js

---
+++ README.md
@@ -0,0 +1,2 @@
+# my-blog
+Blog using Node.js, Express.js and Strapi.
---
+++ services/APIService.js
@@ -0,0 +1,14 @@
+const fetch = require("node-fetch");
+
+let getAllPostsAPI = 'http://localhost:1337/posts';
+
+async function getAPI(url) {
+    console.log(url);
+    const response = await fetch(url);
+    const myJson = await response.json(); 
+    console.log(myJson);
+    return myJson;
+}
+
+module.exports.getAPI = getAPI;
+module.exports.getAllPostsAPI = getAllPostsAPI;
